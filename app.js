14c2fcf2d1f8c040daab9b1dddad5f25210c04b8
NNNiNiNNN <narikinnn@gmail.com>
2022-05-27 07:10:24 +0900

    ポート番号の変更

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const socket_io_client_1 = require("socket.io-client");
-const socket = (0, socket_io_client_1.io)('http://35.193.128.246:' + 5000);
+const socket = (0, socket_io_client_1.io)('http://35.193.128.246:' + 80);
 //const socket = io('http://localhost:' + 5000);
 // 接続時にこのイベントが発火する
 socket.on('connect', () => console.log('connect'));
